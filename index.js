1d6a8f6b4dae7f1aeb776dd7c6770f742e820dee
Pat Cavit <github@patcavit.com>
2016-09-12 11:19:40 -0700

    Better (?) package exports

--- index.js
+++ index.js
@@ -2,4 +2,7 @@
 
 var fs = require("fs");
 
-module.exports = fs.readdirSync("./transforms").map((t) => require(`./transforms/${t}`));
+fs.readdirSync("./transforms").forEach((t) => {
+    exports[t] = require(`./transforms/${t}`);
+});
+
