1bd98d3b88b80412bbba968a36a924e17d653437
Pat Cavit <github@patcavit.com>
2016-09-14 14:40:06 -0700

    Safer checks

--- transforms/m-route-mode/index.js
+++ transforms/m-route-mode/index.js
@@ -11,14 +11,19 @@ var conversion = {
 module.exports = function(file, api) {
     var j = api.jscodeshift,
         s = api.stats;
-
+    
     return j(file.source)
         .find(j.AssignmentExpression)
         .filter((p) => (
-            p.get("left").get("object").get("object").value.name === "m" &&
-            p.get("left").get("object").get("property").value.name === "route" &&
-            p.get("left").get("property").get("name").value === "mode" &&
-            p.get("right").value.value in conversion
+            p.get("left", "object").value &&
+            p.get("left", "object", "object").value &&
+            p.get("left", "object", "object").getValueProperty("name") === "m" &&
+            p.get("left", "object", "property").value &&
+            p.get("left", "object", "property").getValueProperty("name") === "route" &&
+            p.get("left", "property").value &&
+            p.get("left", "property").getValueProperty("name") === "mode" &&
+            p.get("right").value &&
+            p.get("right").getValueProperty("value") in conversion
         ))
         .forEach(() => s("m.route.mode"))
         .replaceWith((p) => j.callExpression(
