ebf9162198d9766e879e8c9f48a48346dcc52c4c
Pat Cavit <github@patcavit.com>
2016-09-14 13:50:50 -0700

    Add stats

--- transforms/m-route-mode/index.js
+++ transforms/m-route-mode/index.js
@@ -9,7 +9,8 @@ var conversion = {
 // https://github.com/lhorie/mithril.js/blob/rewrite/docs/v1.x-migration.md#mroute-mode
 // Converts m.route.mode = "..." calls into m.route.prefix()
 module.exports = function(file, api) {
-    var j = api.jscodeshift;
+    var j = api.jscodeshift,
+        s = api.stats;
 
     return j(file.source)
         .find(j.AssignmentExpression)
@@ -19,6 +20,7 @@ module.exports = function(file, api) {
             p.get("left").get("property").get("name").value === "mode" &&
             p.get("right").value.value in conversion
         ))
+        .forEach(() => s("m.route.mode"))
         .replaceWith((p) => j.callExpression(
             j.memberExpression(
                 j.memberExpression(
