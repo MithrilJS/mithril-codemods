08476f9920199ab29803454d26dde6972173da7b
Pat Cavit <github@patcavit.com>
2016-09-14 11:21:25 -0700

    Safer (maybe?) transform

--- transforms/event-cancel-redraw/index.js
+++ transforms/event-cancel-redraw/index.js
@@ -1,9 +1,12 @@
 "use strict";
 
+var on = /^on/i;
+
 // https://github.com/lhorie/mithril.js/blob/rewrite/docs/v1.x-migration.md#cancelling-redraw-from-event-handlers
 // Converts m.redraw.strategy("none") calls in functions accepting `e` to e.redraw = false;
 module.exports = function(file, api) {
-    var j = api.jscodeshift;
+    var j = api.jscodeshift,
+        s = api.stats;
 
     return j(file.source)
         .find(j.CallExpression)
@@ -19,12 +22,17 @@ module.exports = function(file, api) {
             p.get("arguments", 0).getValueProperty("value") === "none"
         ))
         .replaceWith((p) => {
-            var fn = p,
+            var fn = p.scope.path,
                 arg;
 
-            // Go find the parent function & determine the arg value
-            while(fn.node.type !== "FunctionExpression" && p.parent) {
-                fn = fn.parent;
+            // Go find the nearest scope that is part of a property named "on<something>"
+            while(
+                fn &&
+                !j.Property.check(fn.parent.node) &&
+                fn.parent.get("key").value &&
+                on.test(fn.parent.get("key").getValueProperty("name"))
+            ) {
+                fn = fn.scope.path;
             }
 
             if(!fn) {
@@ -37,6 +45,8 @@ module.exports = function(file, api) {
             if(!arg) {
                 return p;
             }
+            
+            s("m.redraw.strategy(\"none\")");
 
             return j.assignmentExpression(
                 "=",
