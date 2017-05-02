b6c08f6fa5dcc2b615dc15755fb693ea50922b95
Isiah Meadows <impinball@gmail.com>
2017-05-02 11:30:24 -0400

    Update index.js

--- transforms/m-mount-vnodes/index.js
+++ transforms/m-mount-vnodes/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// https://github.com/lhorie/mithril.js/blob/rewrite/docs/change-log.md#passing-vnodes-to-mmount-and-mroute
+// https://mithril.js.org/change-log.md#passing-vnodes-to-mmount-and-mroute
 // Converts raw vnodes passed to m.mount into simple components
 module.exports = (file, api) => {
     var j = api.jscodeshift,
