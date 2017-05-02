fd3f6b48e3351e96358c9deafcce1efc64ffadc8
Isiah Meadows <impinball@gmail.com>
2017-05-02 11:38:53 -0400

    Update index.js

--- transforms/unsafe-component-onunload/index.js
+++ transforms/unsafe-component-onunload/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// https://mithril.js.org/change-log.md#run-code-on-component-removal
+// https://mithril.js.org/change-log.html#run-code-on-component-removal
 // Convert `controller.onunload` into `controller.onremove`
 module.exports = (file, api) => {
     var j = api.jscodeshift,
