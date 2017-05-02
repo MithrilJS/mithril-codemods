1ca88a0f93e874673ab8fda6b2e8078c7125cd30
Isiah Meadows <impinball@gmail.com>
2017-05-02 11:36:43 -0400

    Update index.js

--- transforms/warning-m-redraw-arg/index.js
+++ transforms/warning-m-redraw-arg/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// https://github.com/lhorie/mithril.js/blob/rewrite/docs/change-log.md#synchronous-redraw-removed
+// https://mithril.js.org/change-log.html#synchronous-redraw-removed
 // Add a warning around any usage of `m.redraw()` that accepts an argument
 module.exports = (file, api) => {
     var j = api.jscodeshift,
